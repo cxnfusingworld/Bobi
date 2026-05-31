6d4738e6f60533e5368694d111fb295c83e2c3de
Cxnfusion <cxnfusion.business@gmail.com>
2026-05-31 14:54:37 -0600

    Slash Commands [2]

--- commands/ping.js
+++ commands/ping.js
@@ -1,14 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    // This defines how it looks inside Discord's UI
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
-        
-    // This is what happens when someone runs it
     async execute(interaction) {
-        // Instead of message.reply(), we use interaction.reply()
         await interaction.reply('Pong! 🏓');
     }
 };
