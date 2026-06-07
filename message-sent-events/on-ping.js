b3a1e565382778adc4914b2baeb12668850c0ed4
Cxnfusion <cxnfusion.business@gmail.com>
2026-06-06 21:42:39 -0600

    on-ping smite action

--- message-sent-events/on-ping.js
+++ message-sent-events/on-ping.js
@@ -9,6 +9,20 @@ const messages = [
     "<user> <user> <user> <user>\nyeah how do *YOU* like it??"
 ]
 
+const onSmiteMessages = [
+    "ow",
+    "*dies*",
+    "bro fuck you",
+    "rude :(",
+    "<:why:1513024914314104872>",
+]
+
+function getMessage(from) {
+    let chosenMessage = from[Math.floor(Math.random()*from.length)]
+    chosenMessage = chosenMessage.replaceAll("<user>", `<@${message.author.id}>`)
+    return chosenMessage
+}
+
 module.exports = async function (message) {
     if (message.author.bot) return;
 
@@ -17,10 +31,14 @@ module.exports = async function (message) {
     if (message.mentions.has(botId)) {
         if (message.reference && message.mentions.repliedUser?.id === botId) {
             // On reply
+            const content = mentions.content
+            if (content.find('https://tenor.com/view/worldbox-smited-lightning-gif-17816464421779196148')) {
+                let chosenMessage = getMessage(onSmiteMessages)
+                message.reply(chosenMessage)
+            }
         } else {
-            // On Ping
-            let chosenMessage = messages[Math.floor(Math.random()*messages.length)]
-            chosenMessage = chosenMessage.replaceAll("<user>", `<@${message.author.id}>`)
+            // On ping
+            let chosenMessage = getMessage(messages)
             return await message.reply(chosenMessage);
         }
     }
